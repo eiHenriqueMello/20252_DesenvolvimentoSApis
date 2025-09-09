e7235ef7eae207015f1d8cc6c5b432c1d4af7855
Henrique A de Mello <h03mello@gmail.com>
2025-09-09 11:09:53 -0300

    AULA05 - JSON

--- Aula05/script.js
+++ Aula05/script.js
@@ -1,31 +1,80 @@
 function lerJSON(){
-    const req = new XMLHttpRequest();
+    const req = new XMLHttpRequest()
     req.onreadystatechange = function(){
-        if(this.readyState ==4 && this.status ==200){
+        if( this.readyState == 4 && this.status == 200){
             const divJSON = document.getElementById("divJSON")
-            divJSON.innerHTML = this.responseText + "<br><hr>"
-            const objPessoa = JSON.parse(this.responseText)
+            divJSON.innerHTML = this.responseText + "<BR><hr>"
+            const objPessoa = JSON.parse( this.responseText )
             txt = "Nome: " + objPessoa.nome
-            txt += "<br>Idade: " + objPessoa.idade
-            txt += "<br>Casado(a): " 
+            txt += "<br>Idade:" + objPessoa.idade
+            txt += "<br>Casado(a): "
             //objPessoa.casado ? txt += "Sim" : txt += "Não"
-            if(objPessoa.casado){
-                txt += "Sim" + "<br>Cônjuge: " + objPessoa.conjuge.nome  + "<br>Idade: " + objPessoa.conjuge.idade
+            if( objPessoa.casado ){
+                txt += "Sim" + "<br>Cônjuge: " + objPessoa.conjuge.nome
             }else{
                 txt += "Não"
             }
             txt += "<br>Filhos: "
-            objPessoa.filhos.forEach(filho => {
-              txt += "<br>Nome: " + filho.nome + " - Idade: " + filho.idade  
+            objPessoa.filhos.forEach( filho => {
+                txt += "<br>Nome: " + filho.nome + " - Idade: " + filho.idade
             })
             txt += "<br>Formações: "
-            objPessoa.formacoes.forEach(formacao =>{
+            objPessoa.formacoes.forEach( formacao => {
                 txt += " - " + formacao
             })
             divJSON.innerHTML += txt
         }
     }
 
-    req.open("GET", "dados.json", true)
+    req.open("GET" , "dados.json" , true )
     req.send()
+}
+
+function buscarProdutos(){
+    const req = new XMLHttpRequest()
+    req.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200){
+            const divProdutos = document.getElementById("divProdutos")
+            txt = '<table border="1">'
+            txt += '    <tr> '
+            txt += '        <th>Código</th> '
+            txt += '        <th>Nome</th> '
+            txt += '        <th>Preço</th> '
+            txt += '    </tr> '
+            objJSON = JSON.parse( this.responseText )
+            const produtos = objJSON.produtos
+            produtos.forEach( prod => {
+                txt += "<tr>"
+                txt += "    <td>" + prod.id + "</td>"
+                txt += "    <td>" + prod.nome + "</td>"
+                txt += "    <td>" + prod.preco + "</td>"
+                txt += "</tr>"
+            })
+            txt += "</table>"
+            divProdutos.innerHTML = txt
+        }
+    }
+    req.open("GET" , "servidor.php?buscar" , true )
+    req.send()
+}
+
+function inserirProduto(){
+    const nome = document.getElementById("txtNome").value
+    const preco = document.getElementById("txtPreco").value
+    const req = new XMLHttpRequest()
+    req.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            const json = JSON.parse(this.responseText)
+            txt = json.resposta
+            if(json.id) txt += "\nID gerado: " + json.id
+            alert(txt)
+            buscarProdutos()
+        }
+
+    }
+
+    req.open("POST", "servidor.php?inserir", true)
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+    req.send("name=" + nome + "&price=" + preco)
+
 }
--- Aula05/servidor.php
+++ Aula05/servidor.php
@@ -5,13 +5,40 @@
     $password = "";
     $banco = "loja_25_2";
 
-    $conn = mysqli_connect($local, $user, $password, $banco);
-    if($conn){
-        $query = "SELECT * FROM produto ORDER BY nome";
-        $result = mysqli_query($conn, $query);
-        $linhas = array();
-        while($row = mysqli_fetch_assoc($result) ){
-            $linhas[] = $row;
+    if(isset($_REQUEST['buscar'])){
+
+        $conn = mysqli_connect($local, $user, $password, $banco);
+        if($conn){
+            $query = "SELECT * FROM produto ORDER BY nome";
+            $result = mysqli_query($conn, $query);
+            $linhas = array();
+            while($row = mysqli_fetch_assoc($result) ){
+                $linhas[] = $row;
+            }
+            mysqli_close($conn);
+            echo '{"produtos" : '.json_encode($linhas).'}';
         }
-        echo '{"produtos" : '.json_encode($linhas).'}';
+    }
+
+    if(isset($_REQUEST['inserir'])){
+
+        $nome = $_POST["name"];
+        $preco = $_POST["price"];
+        if($preco =="") 
+            $preco = 0.0;
+        $preco = $preco.replace("," , ".");
+
+        $conn = mysqli_connect($local, $user, $password, $banco);
+        if($conn){
+            $query = "INSERT INTO produto(nome, preco) VALUES('$nome', '$preco') ";
+            mysqli_query($conn, $query);
+            $id = mysqli_insert_id($conn);
+            mysqli_close($conn);
+            if($id > 0 )
+                echo '{"resposta" : "Produto inserido com sucesso" , "id" : '.$id.'}';
+            else 
+                echo'{"resposta" : "Erro ao tentar inserir"}';
+            
+         }else
+            echo '{"resposta" : "Erro ao tentar conectar"}';      
     }
